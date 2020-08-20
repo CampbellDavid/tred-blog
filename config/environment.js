77926b50998eb4aa017675c7e717797b3b22db1f
CampbellDavid <dcampbell092@gmail.com>
2020-08-20 14:16:32 +0100

    revert

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,8 @@
+const env = process.env.NODE_ENV || 'api-development'
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/tred-blog'
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/tred-blog-${env}`
 const secret = process.env.SECRET || 'Secret key'
+
+console.log({ port, dbURI, secret })
 
 module.exports = { port, dbURI, secret }
