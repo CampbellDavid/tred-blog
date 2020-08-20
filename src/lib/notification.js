2a8fb47944a9ef4a4934a780a32e5e33af3d211c
CampbellDavid <dcampbell092@gmail.com>
2020-08-20 13:04:44 +0100

    remove src

--- src/lib/notification.js
+++
@@ -1,7 +0,0 @@
-import { notify } from 'react-notify-toast'
-
-const notification = (message) => {
-	notify.show(message, 'custom', 3000, { background: '#FFFFFF' })
-}
-
-export default notification
